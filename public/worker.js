efd37674ff7ee8562573e0684ff435c3a3b31417
syscallme <139537661+syscallme@users.noreply.github.com>
2022-10-14 06:28:57 +0000

    Update check_process

--- public/worker.js
+++ public/worker.js
@@ -2,4 +2,6 @@ var CACHE_NAME = "machine-learning-resources";
 var urlsToCache = ["/", "/completed"];
 
 // Install a service worker
-self.addEventListener(
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUnti
