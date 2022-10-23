60cf3a06749013e47caf2c6e2bc64f9d76597092
syscallme <139537661+syscallme@users.noreply.github.com>
2022-10-23 18:47:15 +0000

    Remove log line

--- public/worker.js
+++ public/worker.js
@@ -31,4 +31,5 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-   
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist
