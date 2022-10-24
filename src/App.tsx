1f1800600458872af23770a98a1de34136bf5fab
syscallme <139537661+syscallme@users.noreply.github.com>
2022-10-24 14:40:58 +0000

    Scheduler

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,7 @@ const App: React.FC = () => {
       <Main />
       <Footer />
     </div>
+  );
+};
+
+export default App;
---
+++ src/components/CardResource/index.tsx
@@ -0,0 +1,34 @@
+
+import React from "react";
+import { Resource } from "../../types";
+import { Tag } from "../Tag";
+import "./styles.css";
+
+type Props = {
+  resource: Resource;
+  setUrl: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const CardResource = ({ resource, setUrl }: Props) => {
+  return (
+    <div
+      className="card-resource-container"
+      onMouseLeave={() => setUrl("")}
+      onMouseEnter={() => setUrl(resource.link)}
+      onClick={() => window.open(resource.link, "_blank")}
+    >
+      <div className="resource-info">
+        <h3>{resource.title}</h3>
+        <p>{resource.description}</p>
+      </div>
+      <div className="tag-section">
+        <p>Tags:</p>
+        <ul className="tag-list">
+          {resource.tags.map((item, idx) => (
+            <Tag key={idx}>{item}</Tag>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
