71f4c7ec6e04ea60f98e1aed4e50266b910d2ba2
syscallme <139537661+syscallme@users.noreply.github.com>
2022-12-10 09:34:40 +0000

    remove log statement

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,4 +6,8 @@ const Footer: React.FC = () => {
     <footer className="footer">
       ML Resources &#169;{new Date().getFullYear()}, License: &nbsp;
       <a
-        href="https://github.com/Parasgupta44/ml-resources-react/blob/master/LICENSE.tx
+        href="https://github.com/Parasgupta44/ml-resources-react/blob/master/LICENSE.txt"
+        target="blank"
+        rel="noreferrer"
+      >
+   
