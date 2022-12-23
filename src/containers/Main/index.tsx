2155654098dc965e65fdb40180f61114c162353c
syscallme <139537661+syscallme@users.noreply.github.com>
2022-12-23 06:42:23 +0000

    updata readme

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
 import unshuffledResources from "../../ml_resources.json";
 import { useState } from "react";
-import MouseTooltip from "react-sticky-
+import MouseTooltip from "react-sticky-mouse-tooltip";
+import { ReactTinyLink } from "react-tiny-link";
+imp
