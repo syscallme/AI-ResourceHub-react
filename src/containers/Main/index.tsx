4aab0710f42f6eae0e2315ad897e92a52fa6b650
syscallme <139537661+syscallme@users.noreply.github.com>
2022-12-30 07:23:45 +0000

    Committed by npm script.

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -12,4 +12,6 @@ function shuffle<T>(ogArr: Array<T>): Array<T> {
   const arr = [...ogArr];
   for (let i = arr.length - 1; i > 0; i--) {
     const newI = Math.floor(Math.random() * (i + 1));
-   
+    const tempValue = arr[newI];
+    arr[newI] = arr[i];
+    arr[i] 
