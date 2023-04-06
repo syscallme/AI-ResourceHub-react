d2e37378600a21fc0202b3945e2a23df2d2d26ca
syscallme <139537661+syscallme@users.noreply.github.com>
2023-04-06 14:43:21 +0000

    Update map.html

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -4,4 +4,5 @@
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs ope
+// existing tabs open on the page have been closed, since previously cached
+// resour
