5ce546fad82cc53fb6b0ba8c8b339561b19c147a
syscallme <139537661+syscallme@users.noreply.github.com>
2023-04-28 08:57:06 +0000

    Updated pdf

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -15,4 +15,5 @@ const isLocalhost = Boolean(
     // [::1] is the IPv6 localhost address.
     window.location.hostname === "[::1]" ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-
