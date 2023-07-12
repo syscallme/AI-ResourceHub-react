7daaf4ccb6880311c9b727997140f408c75f4fa5
syscallme <139537661+syscallme@users.noreply.github.com>
2023-07-12 15:40:52 +0000

    minor updates.

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -37,4 +37,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.j
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        // This is running on localh
