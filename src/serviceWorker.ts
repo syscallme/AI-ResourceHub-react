0e2c8c4bac4a9b339ed26ebf8ec0f20d5d0837c7
syscallme <139537661+syscallme@users.noreply.github.com>
2023-07-12 19:15:05 +0000

    Fixed buttons

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -40,4 +40,5 @@ export function register(config?: Config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localh
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        
