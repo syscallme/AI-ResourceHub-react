abfbac677a927cdfc7202f39773cbe3699d2668a
syscallme <139537661+syscallme@users.noreply.github.com>
2023-07-19 15:35:13 +0000

    Updated the snowflake-connector-python feedstock.

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -41,4 +41,6 @@ export function register(config?: Config) {
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
-        
+        checkValidServiceWorker(swUrl, config);
+
+        // Add som
