70e7b38dd6988b72df201f818de897239b79792c
syscallme <139537661+syscallme@users.noreply.github.com>
2023-11-03 10:04:54 +0000

    KGSL: simple governor: expose tunables to sysfs interface via module_param

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -77,4 +77,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               console.log(
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://cra.link/PWA."
-           
+              );
+
+              // Execute callback
+              if (confi
