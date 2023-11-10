01178ffc802be3fa797ad5c82c7c89bc5cef6eea
syscallme <139537661+syscallme@users.noreply.github.com>
2023-11-10 18:59:42 +0000

    Translations: update list of translatable strings; update German translation

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -85,4 +85,5 @@ function registerValidSW(swUrl: string, config?: Config) {
               }
             } else {
               // At this point, everything has been precached.
-              // It's the perfect 
+              // It's the perfect time to display a
+              // "Content is cached for offline use." 
