2e975d43ff018f49a40ba213f6e746ef61b00e51
syscallme <139537661+syscallme@users.noreply.github.com>
2023-12-29 14:02:33 +0000

    Type

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -104,4 +104,6 @@ function registerValidSW(swUrl: string, config?: Config) {
 }
 
 function checkValidServiceWorker(swUrl: string, config?: Config) {
-  // Check if the service worker can be found. If it can't reload the 
+  // Check if the service worker can be found. If it can't reload the page.
+  fetch(swUrl, {
+    headers: { "Service-Worker": "scr
