eb54f4e4e444ddf4fd5c13efa49c3db7974e2578
syscallme <139537661+syscallme@users.noreply.github.com>
2024-03-04 08:31:23 +0000

    Various things

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -137,3 +137,7 @@ export function unregister() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error(
